addf3966dfaf0a6e5cbd1ff084ff33999563ad2c
Gaspard <gaspardguidetti@gmail.com>
2025-05-12 16:13:19 +0200

    évaluation

--- chambres/listChambres.php
+++ chambres/listChambres.php
@@ -6,7 +6,22 @@ $stmt = $conn->query("SELECT * FROM chambres ORDER BY numero");
 $chambres = $stmt->fetchAll(PDO::FETCH_ASSOC);
 closeDatabaseConnection($conn);
 
-
+ // Gestion des messages d'erreurs
+ if (isset($_GET['message'])) {
+    $message = htmlspecialchars(urldecode($_GET['message'])); // limiter les injections XSS
+   
+    if (strpos($message, 'ERREUR') !== false) {
+    echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>"
+    .$message
+    ."<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>"
+    ."</button></div>";
+    } else {
+    echo "<div class='alert alert-warning alert-dismissible fade show' role='alert'>"
+    .$message
+    ."<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'>"
+    ."</button></div>";
+    }
+    }
 
 ?>
 
